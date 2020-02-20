b62d9464397f985b53674e5a2276c18d38a46fde
Victor Luiz <victorlrosa@hotmail.com>
2020-02-20 19:21:26 -0300

    Criando a classe CoordenadasXadrez.

--- Chess-ConsoleApp/Program.cs
+++ Chess-ConsoleApp/Program.cs
@@ -12,20 +12,29 @@ namespace Chess_ConsoleApp
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
 
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+            CoordenadasXadrez cx = new CoordenadasXadrez('a', 1);
+            Console.WriteLine(cx);
 
-                Tela.Imprimirtabuleiro(tab);
-            }
-            catch(TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(cx.ToPosicao());
+            
+            
+            
+            
+            //try
+            //{
+            //    Tabuleiro tab = new Tabuleiro(8, 8);
+
+            //    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            //    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            //    tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
+            //    Tela.Imprimirtabuleiro(tab);
+            //}
+            //catch(TabuleiroException e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
             Console.ReadKey();
         }
     }
---
+++ Chess-ConsoleApp/Xadrez/CoordenadasXadrez.cs
@@ -0,0 +1,26 @@
+﻿using Chess_ConsoleApp.Mesa;
+
+namespace Chess_ConsoleApp.Xadrez
+{
+    class CoordenadasXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public CoordenadasXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}
